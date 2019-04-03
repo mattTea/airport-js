57bce2aa82accd16c95915cd8f1acf955011117e
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-04-03 09:54:48 +0100

    Passes first isFlying() test

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -1,6 +1,12 @@
 describe("Plane", function() {
   it("responds to land function", function() {
     plane = new Plane();
-    expect(plane.land).not.toBe(undefined)
+    expect(plane.land).not.toBe(undefined);
+  });
+
+  it("changes state of plane from flying to landed", function() {
+    plane = new Plane();
+    plane.land();
+    expect(plane.isFlying()).toEqual(false);
   });
 });
--- src/Plane.js
+++ src/Plane.js
@@ -3,5 +3,9 @@ function Plane() {
 }
 
 Plane.prototype.land = function() {
-  
+
+}
+
+Plane.prototype.isFlying = function() {
+  return false
 }
