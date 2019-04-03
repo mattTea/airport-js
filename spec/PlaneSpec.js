63f73a8bfbc35744ccbd83d241c2b6d54402c330
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-04-03 10:11:32 +0100

    Refactors last commit

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -1,17 +1,25 @@
 describe("Plane", function() {
-  it("responds to land function", function() {
-    plane = new Plane();
-    expect(plane.land).not.toBe(undefined);
-  });
+  var plane;
 
-  it("changes state of plane from flying to landed", function() {
-    plane = new Plane();
-    plane.land();
-    expect(plane.isFlying()).toEqual(false);
+  beforeEach(function() {
+    plane = new Plane;
   });
-
+  
   it("isFlying() when initialised", function() {
     plane = new Plane();
     expect(plane.isFlying()).toEqual(true);
+  });
+  
+  describe("when instructed to land", function() {
+    it("responds to land function", function() {
+      plane = new Plane();
+      expect(plane.land).not.toBe(undefined);
+    });
+
+    it("changes flightStatus from flying to landed", function() {
+      plane = new Plane();
+      plane.land();
+      expect(plane.isFlying()).toEqual(false);
+    });
   });
 });
