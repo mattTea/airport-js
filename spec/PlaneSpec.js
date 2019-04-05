9c1bdc07b8302a49bfc789d747739e37e6fb923f
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-04-05 15:42:50 +0100

    User story 3: Prevent take-off if stormy (WIP - checks weather when instructed to land)
    As an air traffic controller
    To ensure safety
    I want to prevent takeoff when weather is stormy

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -15,43 +15,51 @@ describe("Plane", function() {
   
   describe("when instructed to land", function() {
     it("changes flightStatus from flying to landed", function() {
-      plane.land(airport);
+      plane.land(airport, weather);
       expect(plane.isFlying()).toEqual(false);
     });
 
     it("calls airport.landPlane()", function() {
-      plane.land(airport);
+      plane.land(airport, weather);
       expect(airport.landPlane).toHaveBeenCalledWith(plane);
+    });
+
+    it("checks weather", function() {
+      plane.land(airport, weather);
+      expect(weather.isStormy).toHaveBeenCalled();
     });
   });
   
   describe("when instructed to take-off", function() {
-    it("changes flightStatus from flying to landed", function() {
-      plane.land(airport);
-      expect(plane.isFlying()).toEqual(false);
-      weather.isStormy.and.returnValue(false);
-      plane.takeOff(airport, weather);
-      expect(plane.isFlying()).toEqual(true);
-    });
-
-    it("calls airport.launchPlane()", function() {
-      plane.land(airport);
-      weather.isStormy.and.returnValue(false);
-      plane.takeOff(airport, weather);
-      expect(airport.launchPlane).toHaveBeenCalledWith(plane);
+    beforeEach(function() {
+      plane.land(airport, weather)
     });
 
     it("checks weather", function() {
-      plane.land(airport);
-      weather.isStormy.and.returnValue(false);
       plane.takeOff(airport, weather);
       expect(weather.isStormy).toHaveBeenCalled();
     });
 
     it("prevents take-off when weather is stormy", function() {
-      plane.land(airport);
       weather.isStormy.and.returnValue(true);
       expect(function(){ plane.takeOff(airport, weather) }).toThrow(new Error("Take-off is not possible in this storm."));
+    });
+
+    describe("when weather is not stormy", function() {
+      beforeEach(function() {
+        weather.isStormy.and.returnValue(false);
+      });
+      
+      it("changes flightStatus from landed to flying", function() {
+        expect(plane.isFlying()).toEqual(false);
+        plane.takeOff(airport, weather);
+        expect(plane.isFlying()).toEqual(true);
+      });
+  
+      it("calls airport.launchPlane()", function() {
+        plane.takeOff(airport, weather);
+        expect(airport.launchPlane).toHaveBeenCalledWith(plane);
+      });
     });
   });
 });
--- src/Plane.js
+++ src/Plane.js
@@ -2,16 +2,17 @@ function Plane() {
   this._inFlight = true;
 }
 
-Plane.prototype.land = function(airport = new Airport) {
-  // guard clause -> return something if isStormy()
-  airport.landPlane(this);
+Plane.prototype.land = function(airport = new Airport, weather = new Weather) {
+  if (weather.isStormy()) {
+    throw new Error("Landing is not possible in this storm.");
+  } else {
+    airport.landPlane(this);
   this._inFlight = false;
+  }
 }
 
 Plane.prototype.takeOff = function(airport = new Airport, weather = new Weather) {
-  // guard clause -> return something if isStormy()
-  var stormCheck = weather.isStormy()
-  if (stormCheck) {
+  if (weather.isStormy()) {
     throw new Error ("Take-off is not possible in this storm.");
   } else {
     airport.launchPlane(this);
